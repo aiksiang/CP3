691832f3682b79a2631467a4165686805439f591
MartinGaoR <a0101891@u.nus.edu>
2015-03-18 02:22:33 +0800

    Create Errata.php

---
+++ php/errata_store.php
@@ -0,0 +1,15 @@
+
+<?php
+//The errata table should consist 
+//	serverity_level
+//	type
+//	page number
+//  location
+//  corrected content
+//  status (fixed/not fixed)
+//  author_id 
+//	raise_time
+//  fix_time
+//  admin_comment
+//
+?>
