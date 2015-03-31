73e43ff11f67e586a83f2670b027250c5540e072
Aik Siang <ang.aik.siang@gmail.com>
2015-03-31 17:38:58 +0800

    Added Errata into the system

--- js/errata.js
+++ js/errata.js
@@ -6,11 +6,61 @@ function showErrata(edition) {
 		errata = result;
 
 		$(".errata").html = ("");
-		var HTMLtoBeInserted = '\
+		var HTMLtoBeInserted = '';
+		HTMLtoBeInserted += '\
+			<div class="container">\
+				<div class="col-md-12" id="report-error">\
+					<div class="row">\
+						<div class="col-md-12" id="select-edition"><h3>Report Error</h3></div>\
+						<p>This page contains all the known errors and bugs for <span id="edition">CP</span>. <br>Please report any sightings of bugs, errors or misprints here.</p>\
+					</div>\
+					\
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#errata-submit">Report</button>\
+		  		</div>\
+		  	</div>\
+		  	<hr/>\
+		';
+		HTMLtoBeInserted += '\
+			<div class="container">\
+				<div class="row">\
+					<div class="col-md-12" id="orderby"><h3>View Errata by Order</h3></div>\
+				</div>\
+				<div class="row">\
+					<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
+					<div class="col-md-2"><h5>Date of posting </h5></div>\
+					<div class="col-md-2"><input checked type="radio" value="date-most" name="check">&nbsp;&nbsp;Most Recent</div>\
+					<div class="col-md-2"><input type="radio" value="date-less" name="check">&nbsp;&nbsp;Least Recent</div>\
+					<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
+				</div>\
+				<div class="row">\
+					<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
+					<div class="col-md-2"><h5>Page Number </h5></div>\
+					<div class="col-md-2"><input type="radio" value="pg-asc" name="check">&nbsp;&nbsp;Ascending</div>\
+					<div class="col-md-2"><input type="radio" value="pg-desc" name="check">&nbsp;&nbsp;Descending</div>\
+					<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
+				</div>\
+				<div class="row">\
+					<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
+					<div class="col-md-2"><h5>Severity </h5></div>\
+					<div class="col-md-2"><input type="radio" value="sev-most" name="check">&nbsp;&nbsp;Most Severe</div>\
+					<div class="col-md-2"><input type="radio" value="sev-less" name="check">&nbsp;&nbsp;Least Severe</div>\
+					<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
+				</div>\
+				<div class="row">\
+					<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
+					<div class="col-md-2"><h5>Reporter\'s Name </h5></div>\
+					<div class="col-md-2"><input type="radio" value="atoz" name="check">&nbsp;&nbsp;A to Z</div>\
+					<div class="col-md-2"><input type="radio" value="ztoa" name="check">&nbsp;&nbsp;Z to A</div>\
+					<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
+				</div>\
+			</div>\
+		  	<hr/>\
+		';
+		HTMLtoBeInserted += '\
 			<table class="table">\
 				<tr>\
 					<th>Page Number</th>\
-					<th>Errata</th>\
+					<th width="1500px">Errata</th>\
 					<th>Type</th>\
 					<th>Status</th>\
 				</tr>\
