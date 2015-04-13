b8868e7a932a270edd2c3da04f0c6d163bdbfd16
ycewe <theericewe@live.com>
2015-04-13 17:07:20 +0800

    Added back to menu button, added facebook like, change link color

--- js/testimonial.js
+++ js/testimonial.js
@@ -91,6 +91,13 @@ function showTestimonial() {
   			}
   		}
   	}
+
+  	HTMLtoBeInserted += '\
+		<br>\
+		<div class="btn-group backToTop">\
+  			<a href="#content-top"><button type="button" class="btn btn-default center-block"><span class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span> Back to Menu</button></a>\
+  		</div>\
+  	';
   	
 	$(".testimonial").html(HTMLtoBeInserted);
 }
