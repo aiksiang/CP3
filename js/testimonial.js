c3a142865a4e96aa43dd462a6a56f1f21abf88d3
Aik Siang <ang.aik.siang@gmail.com>
2015-05-06 23:43:37 +0800

    add admin page

--- js/testimonial.js
+++ js/testimonial.js
@@ -29,27 +29,7 @@ function showTestimonial() {
 			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#review-submit" onClick="getQuestion();">Review</button>\
   		</div>\
   	';
-  // 	HTMLtoBeInserted += '\
-  // 		<div class="row">\
-		// 	<div class="col-md-12" id="orderby"><h3>Order by</h3></div>\
-		// </div>\
-		// <div class="row">\
-		// 	<div class="row">\
-		// 	<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
-		// 	<div class="col-md-2"><h5>Country </h5></div>\
-		// 	<div class="col-md-2"><input checked type="radio" value="country-atoz" name="check"><span>&nbsp;&nbsp;A to Z</span></div>\
-		// 	<div class="col-md-2"><input type="radio" value="country-ztoa" name="check"><span>&nbsp;&nbsp;Z to A</span></div>\
-		// 	<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
-		// 	</div>\
-		// 	<div class="row">\
-		// 	<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
-		// 	<div class="col-md-2"><h5>Reviewer\'s Name </h5></div>\
-		// 	<div class="col-md-2"><input type="radio" value="rev-atoz" name="check"><span>&nbsp;&nbsp;A to Z</span></div>\
-		// 	<div class="col-md-2"><input type="radio" value="rev-ztoa" name="check"><span>&nbsp;&nbsp;Z to A</span></div>\
-		// 	<div class="col-md-3"><div class="row"><div class="classWithPad"></div></div></div>\
-		// 	</div>\
-		// </div>\
-  // 	';
+
   	for (var region in testimonials) {
   		HTMLtoBeInserted += '\
 			<h1 class="row">\
